6c7b5a0bba8a35d1a61c22d2e183c7fa7d22c337
balkon16 <pawel.k.lonca@gmail.com>
2021-10-23 21:11:19 +0200

    work in progress

--- src/zad2/Main.groovy
+++ src/zad2/Main.groovy
@@ -1,10 +1,15 @@
 /**
  *
- *  @author Lonca Paweł S23452
+ * @author Lonca Paweł S23452
  *
  */
 
-package zad2;
+// TODO: funkcja do parsowania
+// TODO: funkcja do dodawania w ramach grupy -> coś sprytnego z programowania funkcyjnego
+// TODO: zaprojektować ekran do wprowadzania -> https://docs.oracle.com/javase/tutorial/uiswing/components/dialog.html
+//  > dwa przyciski: Dalej, Podlicz
 
+package zad2
 
- 
+import javax.swing.JOptionPane;
+

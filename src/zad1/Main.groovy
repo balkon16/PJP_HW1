baa8bf6bb247b3d664f617d350460f6da48daf6c
balkon16 <pawel.k.lonca@gmail.com>
2021-10-23 20:45:32 +0200

    Task no. 1 done

--- src/zad1/Main.groovy
+++ src/zad1/Main.groovy
@@ -7,31 +7,27 @@
 package zad1
 
 
-import javax.swing.JOptionPane;
+import javax.swing.JOptionPane
 
 class Numbers {
     static final numberToWordMap = ['0': 'zero', '1': 'jeden', '2': 'dwa', '3': 'trzy', '4': 'cztery', '5': 'pięć', '6': 'sześć',
                                     '7': 'siedem', '8': 'osiem', '9': 'dziewięć']
 }
 
-
 static boolean isValidInput(String input) {
     if ((input == '') | (input == null)) return false
     def allowedCharacters = Numbers.numberToWordMap.keySet().toSet()
-    for (String c: input.toList()) {
-        if (! allowedCharacters.contains(c)) return false
+    for (String c : input.toList()) {
+        if (!allowedCharacters.contains(c)) return false
     }
     return true
 }
 
 static String getTransformedInput(String input) {
-    // TODO: napisać funkcję
-
-    return null
+    return input.toList().collect { Numbers.numberToWordMap.get(it) }.join(' - ')
 }
 
-// TODO: przerwij działanie programu, gdy nieprawidłowy input
-//isValidInput('1234')
 
-//String userInput = JOptionPane.showInputDialog("Specify a number")
-//println isValidInput(userInput)
+String userInput = JOptionPane.showInputDialog("Specify a number:")
+if (isValidInput(userInput)) println(getTransformedInput(userInput))
+else println("Invalid input: $userInput")
--- src/zad1/tests/unit/TransformInputCase.groovy
+++ src/zad1/tests/unit/TransformInputCase.groovy
@@ -1,11 +1,14 @@
 package zad1.tests.unit
 
 import groovy.test.GroovyTestCase
+import zad1.Main
 
 class TransformInputCase extends GroovyTestCase {
 
     void testAssertions() {
-        // TODO: napisać testy
-
+        assertTrue(Main.getTransformedInput('147') == 'jeden - cztery - siedem')
+        assertTrue(Main.getTransformedInput('0') == 'zero')
+        assertTrue(Main.getTransformedInput('10') == 'jeden - zero')
+        assertTrue(Main.getTransformedInput('96') == 'dziewięć - sześć')
     }
 }

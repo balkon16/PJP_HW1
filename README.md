d434d7364a6cb9fecd2078ea4c1a670cbb649a58
balkon16 <pawel.k.lonca@gmail.com>
2021-10-31 13:57:05 +0100

    task completed

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ Solution for the exercise no. 1 is located in the `src/zad1` package. The aim of
 2. Print out the name of each number. Words should be separated by a dash.
 
 # Exercise no. 2
+
+Solution for the exercise no. 2 is located in the `src/zad2` package. The aim of the exercise is to:
+1. Get user's input. User is supposed to provide 0 or more strings *product = price* where the *product* is a sequence of characters and 
+the *price* can be specified either as an integer or a floating-point number.
+2. Once the user stops adding new entries (by clicking the Cancel button) a summary of products is computed. Entries are
+grouped by the product's name and prices within the group are summed up.
--- src/zad2/Main.groovy
+++ src/zad2/Main.groovy
@@ -4,10 +4,6 @@
  *
  */
 
-// TODO: funkcja do dodawania w ramach grupy -> coś sprytnego z programowania funkcyjnego
-// TODO: zaprojektować ekran do wprowadzania -> https://docs.oracle.com/javase/tutorial/uiswing/components/dialog.html
-//  > dwa przyciski: Dalej, Podlicz
-
 package zad2
 
 import javax.swing.JOptionPane;
@@ -20,4 +16,18 @@ static validateAndTransformUserInput(String userInput) {
     else return new Tuple2<>(false, null)
 }
 
-println(validateAndTransformUserInput("chleb = 10,51"))
+def productsSummary = [:]
+while (true) {
+    String productPrice = JOptionPane.showInputDialog("Specify a product and its price. \nClick OK to add an entry, click Cancel to get a summary.")
+    def parsedInput = validateAndTransformUserInput(productPrice)
+    if (parsedInput[0]) {
+        def productName = parsedInput[1][0]
+        productsSummary[productName] = productsSummary.get(productName, 0) + parsedInput[1][1]
+    }
+    if (productPrice == null) {
+        println("Making a summary of your products...")
+        break
+    }
+}
+
+productsSummary.sort().eachWithIndex { entry, i -> println "${i + 1}. $entry.key: $entry.value" }
